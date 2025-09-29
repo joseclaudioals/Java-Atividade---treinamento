df94f2b9d122ed058a046f06f0682825df4af9b6
joseclaudioals <joseclaudioals1092gmail.com>
2025-09-29 12:45:23 -0300

    questão 19 finalizada

--- Lista_1/questao19.java
+++ Lista_1/questao19.java
@@ -15,6 +15,24 @@ public class questao19 {
         System.out.println("insira um ano");
         ano = sc.nextInt();
         
+        boolean verificador=true;
+
+        if(mes<=0 && mes>12 || dia<=0 && dia>31 || ano<0)
+            verificador = false;
+            else if((mes == 4 || mes == 6 || mes == 9 || mes == 11)&&dia>30)
+                verificador = false;
+        if(mes == 2){
+            // verificação se um mes eh bissexto == (ano % 400 == 0) || (ano % 4 == 0 && ano % 100 != 0)
+            if(((ano % 400 == 0) || (ano % 4 == 0 && ano % 100 != 0)) && dia>29)
+                verificador = false;
+                else if(!((ano % 400 == 0) || (ano % 4 == 0 && ano % 100 != 0))&& dia>28)
+                    verificador = false;
+        }
+
+        if(verificador)
+            System.out.println("valido");
+            else 
+                System.out.println("invalido");
         
     }
 }
