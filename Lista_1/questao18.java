28ec815591ab88863e40e15dba444be95827473e
joseclaudioals <joseclaudioals1092gmail.com>
2025-09-29 12:24:18 -0300

    questão 18 completa, README.md e questao 19 incompleta

---
+++ Lista_1/questao18.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class questao18 {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        String sInicial="", sFinal="";
+
+        System.out.println("- VERIFICADOR DE PALINDROMO -");
+        System.out.println("insira uma frase para verifiação");
+        sInicial = sc.nextLine();
+        sInicial=sInicial.replaceAll("[^a-zA-Z]", "").toLowerCase();
+        
+        for(int i = 0; i<sInicial.length(); i++){
+            sFinal = sInicial.charAt(i) + sFinal;
+        }
+
+        if(sInicial.equals(sFinal))
+            System.out.println("a frase eh um palindromo");
+            else 
+                System.out.println("a palavra NAO eh um palindromo");
+    }
+}
---
+++ Lista_1/questao19.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class questao19 {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+
+        int dia, mes, ano;
+        System.out.println("- VALIDADOR DE DATAS -");
+        System.out.println("insira um dia");
+        dia = sc.nextInt();
+
+        System.out.println("insira um mes");
+        mes = sc.nextInt();
+
+        System.out.println("insira um ano");
+        ano = sc.nextInt();
+        
+        
+    }
+}
